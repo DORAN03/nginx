7d1e991f42de84357395e97b009a50119ef0b456
DORAN03 <37197784+DORAN03@users.noreply.github.com>
2022-05-20 08:35:49 -0400

    Update display.php

--- site/display.php
+++ site/display.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <title>Event Display || STA Tech Management</title>
-    <link rel="stylesheet" href="Display.css">
+    <link rel="stylesheet" href="display.css">
   </head>
   <body>
     <iframe src="https://challonge.com/74ottot8/module?show_final_results=1&scale_to_fit=1&show_tournament_name=1" width="100%" height="85%" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
