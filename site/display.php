866a6b8586027c31be02da8acae587e2560e7c9c
DORAN03 <37197784+DORAN03@users.noreply.github.com>
2022-05-20 08:34:46 -0400

    Create display.php

---
+++ site/display.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Event Display || STA Tech Management</title>
+    <link rel="stylesheet" href="Display.css">
+  </head>
+  <body>
+    <iframe src="https://challonge.com/74ottot8/module?show_final_results=1&scale_to_fit=1&show_tournament_name=1" width="100%" height="85%" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+    <div class="cover"></div>
+
+    <div class="Announcement">
+      <h2 class="AlertTitle">Admin Override || Announcement Override</h2>
+      <p class="AlertText">This is a test of the emergency alert system. This is only a test. There is no cause for panic. Thank you.</p>
+    </div>
+    
+  </body>
+</html>
