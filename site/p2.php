5455dd4876e55010f75c267e981a1a7cdd78cf7a
DORAN03 <37197784+DORAN03@users.noreply.github.com>
2022-05-26 09:10:31 -0400

    Create p2.php

---
+++ site/p2.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+  <title>P2Design || Home</title>
+  <link rel="stylesheet" href="home.css">
+</head>
+   <body>
+    <div class="NavBar">
+      <ul class="NavUL">
+        <li class="NavLI-L" id="logo"><img id="logo" src="./P2Design.png" alt="P2Design Logo"></li>
+        <li class="NavLI" id="enabled"><a href="learn.html">Contact</a></li>
+        <li class="NavLI" id="enabled"><a href="learn.html">About</a></li>
+        <li class="NavLI" id="disabled"><a>Shop</a></li>
+        <li class="NavLI" id="enabled"><a>Posts</a></li>
+      </ul>
+    </div>
+    <div class="break">
+    </div>
+
+    <div class="TitleBubble1">
+    </div>
+
+    <div class="circle1">
+    </div>
+    <div class="circle2">
+    </div>
+    <div class="circle3">
+    </div>
+    <div class="circle4">
+    </div>
+    <div class="circle5">
+    </div>
+    <div class="circle6">
+    </div>
+    <div class="circle7">
+    </div>
+    <div class="circle8">
+    </div>
+    <div class="circle9">
+    </div>
+  </body>
+  
+</html>
